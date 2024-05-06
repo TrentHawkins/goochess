dda2d983b1a5c77fe28d2b33ff437671f0644048
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2024-05-06 16:02:02 +0300

    Complete closure on squares

--- board.py
+++ board.py
@@ -1,51 +1,52 @@
 from re import compile, Pattern
+from typing import Self
 
 
 class Index(int):
 
-	lower_bound: int =  0
-	upper_bound: int =  8
+	lower: int =  0
+	upper: int =  8
 
 
-	def __new__(cls, coordinate: int):
-		if cls.lower_bound <= coordinate < cls.upper_bound:
-			return super().__new__(cls, coordinate)
+	def __new__(cls, index: int) -> Self:
+		if cls.lower <= index < cls.upper:
+			return super().__new__(cls, index)
 
-		raise IndexError("coordinate out of chessboard bounds")
+		raise IndexError(f"{cls.__name__.lower()} out of chessboard bounds")
 
 
-	def __add__(self, other):
+	def __add__(self, other) -> Self:
 		return self.__class__(super().__add__(other))
 
-	def __sub__(self, other):
+	def __sub__(self, other) -> Self:
 		return self.__class__(super().__sub__(other))
 
 
 class Summable:
 
-	def __radd__(self, other):
+	def __radd__(self, other) -> Self:
 		return self + other if other else self
 
-	def __rsub__(self, other):
+	def __rsub__(self, other) -> Self:
 		return self - other if other else self
 
-	def __rmul__(self, other):
+	def __rmul__(self, other) -> Self:
 		return self * other if other else self
 
 
 class BiIndex(Index, Summable):
 
-	lower_bound: int = -7
-	upper_bound: int = +8
+	lower: int = -7
+	upper: int = +8
 
 
-	def __mul__(self, other):
+	def __mul__(self, other) -> Self:
 		return self.__class__(super().__mul__(other))
 
-	def __pos__(self):
+	def __pos__(self) -> Self:
 		return self.__class__(super().__pos__())
 
-	def __neg__(self):
+	def __neg__(self) -> Self:
 		return self.__class__(super().__neg__())
 
 
@@ -56,35 +57,35 @@ class Vector(Summable,
 	]
 ):
 
-	def __new__(cls, *coordinates):
-		return super().__new__(cls, coordinates)
+	def __new__(cls, *indices) -> Self:
+		return super().__new__(cls, indices)
 
 
-	def __add__(self, other):
+	def __add__(self, other) -> Self:
 		return self.__class__(
 			self[0] + other[0],
 			self[1] + other[1],
 		)
 
-	def __sub__(self, other):
+	def __sub__(self, other) -> Self:
 		return self.__class__(
 			self[0] - other[0],
 			self[1] - other[1],
 		)
 
-	def __mul__(self, other):
+	def __mul__(self, other) -> Self:
 		return self.__class__(
 			self[0] * other,
 			self[1] * other,
 		)
 
-	def __pos__(self):
+	def __pos__(self) -> Self:
 		return self.__class__(
 			+self[0],
 			+self[1],
 		)
 
-	def __neg__(self):
+	def __neg__(self) -> Self:
 		return self.__class__(
 			-self[0],
 			-self[1],
@@ -103,37 +104,36 @@ class Square(
 
 	notation: Pattern = compile(f"[{files}][{ranks}]")
 
-	def __new__(cls, square: str | tuple):
-		if isinstance(square, str):
-			if cls.notation.fullmatch(square):
-				return super().__new__(cls,
-					(
-						Index( 8 - int(square[1])),
-						Index(ord(square[0]) - 97),
-					)
-				)
 
-		if isinstance(square, tuple):
-			return super().__new__(cls, square)
+	def __new__(cls, *indices: Index) -> Self:
+		return super().__new__(cls, indices)
+
+
+	def __repr__(self):
+		return self.file + self.rank
+
+	def __add__(self, other: Vector) -> Self:
+		return self.__class__(
+			self[0] - other[0],
+			self[1] + other[1],
+		)
+
+	def __sub__(self, other: Self) -> Vector:
+		return Vector(
+			BiIndex(other[0]) - self[0],
+			BiIndex(self[1]) - other[1],
+		)
+
+
+	@classmethod
+	def from_notation(cls, square: str) -> Self:
+		if cls.notation.fullmatch(square):
+			return cls(
+				Index( 8 - int(square[1])),
+				Index(ord(square[0]) - 97),
+			)
 
 		raise IndexError("invalid square")
-
-
-	def __add__(self, other: Vector):
-		return self.__class__(
-			(
-				self[0] - other[0],
-				self[1] + other[1],
-			)
-		)
-
-	def __sub__(self, other: Vector):
-		return self.__class__(
-			(
-				self[0] + other[0],
-				self[1] - other[1],
-			)
-		)
 
 
 	@property
@@ -141,5 +141,5 @@ class Square(
 		return chr(int(self[1]) + 97)
 
 	@property
-	def rank(self) -> int:
-		return int( 8 - int(self[0]))
+	def rank(self) -> str:
+		return str( 8 - int(self[0]))
