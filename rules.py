d7b5d51828b6fbe993ba855e8949f09cbf4ec45e
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2025-02-21 13:14:09 +0000

    Add representation details

--- rules.py
+++ rules.py
@@ -7,7 +7,7 @@ from weakref import ref as weakref
 from chess.geometry import Square
 
 if TYPE_CHECKING:
-	from chess.material import Piece, Pawn, Officer
+	from chess.material import Piece, Officer, Pawn, Rook, King
 
 
 class Move:
@@ -24,26 +24,54 @@ class Move:
 		assert (board := self.piece.game()) is not None
 		return (other := board[self.target]) is     None
 
+	def __repr__(self) -> str:
+		return repr(self.piece) + repr(self.source) + "-" + repr(self.target)
+
 
 class Capt(Move):
+
+	def __repr__(self) -> str:
+		return super().__repr__().replace("-", "×")
 
 	def __bool__(self) -> bool:
 		assert (board := self.piece.game()) is not None
 		return (other := board[self.target]) is not None and self.piece.color != other.color
 
 
-class Promote(Move):
+class Dependent(Move):
+
+	def __init__(self, piece: Piece, square: Square, dependant: Piece):
+		super().__init__(piece, square)
+
+		self.dependant = dependant
+
+
+class Promote(Dependent):
 
 	def __init__(self, pawn: Pawn, square: Square, officer: Officer):
-		super().__init__(pawn, square)
+		super().__init__(pawn, square, officer)
 
-		self.officer = officer
+	def __repr__(self) -> str:
+		return super().__repr__() + repr(self.dependant)
 
 	def __bool__(self) -> bool:
 		assert (board := self.piece.game()) is not None
-		return self.officer.square is None and super().__bool__()
+		return self.dependant.square is None and super().__bool__()
 
 
-class Castle(Move):
+class Castle(Dependent):
 
-	...
+	def __init__(self, king: King, square: Square, rook: Rook):
+		super().__init__(king, square, rook)
+
+
+class CastleL(Castle):
+
+	def __repr__(self) -> str:
+		return "O-O-O"
+
+
+class CastleS(Castle):
+
+	def __repr__(self) -> str:
+		return "O-O"
